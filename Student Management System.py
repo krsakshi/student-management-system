2e1056578e6598e6fa376bf333d2346f4a2893c6
Sakshi <thekrsakshi@gmail.com>
2020-08-18 19:08:30 +0530

    Update Student Management System Program

---
+++ Student Management System.py
@@ -0,0 +1,10 @@
+print("\n\nStudent Management System \n\n")
+
+c=1         # For exiting main loop
+n = []      # Name list
+r = []      # Roll No list
+b = []      # Branch list
+p = []      # Phone No list
+a = []      # Address list
+g = []      # Grade list
+
