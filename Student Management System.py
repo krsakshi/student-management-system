e0f03c2b46c5667d216f13c87fbfd5cee4215924
Sakshi <thekrsakshi@gmail.com>
2020-08-20 19:09:19 +0530

    Update Student Management System Program

--- Student Management System.py
+++ Student Management System.py
@@ -8,3 +8,14 @@ p = []      # Phone No list
 a = []      # Address list
 g = []      # Grade list
 
+while(c==1):      # Loop will continue until the value of c not equal to 1 (Helps in Exit choise)
+
+    print("INSTRUCTIONS:")              # Instrunctions
+    print("Press:1 To Add a Student")
+    print("Press:2 To View a Student Details")
+    print("Press:3 To View all Students Details")
+    print("Press:4 To Delete a Student Record")
+    print("Press:5 to Update Student's Info")
+    print("Press:6 to Exit")
+
+    
